214ed833e92dd9f484bc67bfa267000239f7d8af
giarengo <80314530+giarengo@users.noreply.github.com>
2023-09-15 17:57:02 +0200

    Fix plot_athena.py

--- plot_athena.py
+++ plot_athena.py
@@ -1,14 +1,17 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt 
- 
-# Create pd dataframe from Athena output file 
-file = r'./GeGa_vs_SiGeGa.chi'
-with open(file) as f:
-    l = f.readlines() 
-for i, line in enumerate(l): 
-    if line.find('#') == -1:
-        break 
-l[i-1] = l[i-1].replace('#','')
-l = [line.split() for line in l[i-1:]]
-df = pd.DataFrame(l[1:], columns=l[0])
+import pandas as pd 
+
+# Athena / Artemis parser
+def parser(filename):
+    with open(filename) as file: 
+        l = file.readlines() 
+
+    for i, line in enumerate(l): 
+        if line.find('#') != -1: 
+            l[i] = l[i].replace('#','')
+        if line.find('---') != -1:
+            i_data = i
+
+    file = StringIO(''.join(l[i_data+1:]))
+    df = pd.read_csv(file, sep='\s+')
+
+    return df
